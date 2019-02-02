ce965829b119c2a2812d37888c133351512ebc07
Onno <onno.kampman@gmail.com>
2019-02-02 19:02:17 +0800

    add main consumer demo script

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Demo script for analyzing personality based on language usage.
+"""
+
+query = input('Enter sentence to find personality: ')  # returns a string
+
+print("Returning personality for sentence '%s'" % query)
